9bb54b47f0338f76136526007fe54cb375aae3b3
Sanket <sanketwakhare@gmail.com>
2021-09-04 11:38:22 +0530

    wire output text to div container

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const buttonTranslate = document.querySelector("#btn_translate");
 const textAreaInput = document.querySelector("#txtArea_text");
+const translatedTextContainer = document.querySelector("#translated_text");
 let inputText;
 
 buttonTranslate.addEventListener("click", () => {
   inputText = textAreaInput.value;
   console.log(inputText);
+
+  translatedTextContainer.innerHTML = inputText;
 });
